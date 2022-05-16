78d1f590f641bbdb00a3717326a8c55ade1afacf
JackSchreuder <jackschreuder@icloud.com>
2022-05-16 12:40:11 -0700

    messing around a little

--- summary_calc.r
+++ summary_calc.r
@@ -1,1 +1,7 @@
 # This Doc for calculating summary info
+library(dplyr)
+
+park_visits <- read.csv("park_visits.csv")
+park_visits <- park_visits %>% mutate(visitors_millions = (10^logVisits)/1000000)
+by_park_year <- park_visits %>% group_by(ParkName, Year) %>% summarise(total_vis = sum(visitors_millions))
+vis_by_region <- park_visits %>% group_by(Region) %>% summarise(total_vis = sum(visitors_millions))
