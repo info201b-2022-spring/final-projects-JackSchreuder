3c8f3d7b6c62edcc6409abd22b6f658309e38687
JackSchreuder <jackschreuder@icloud.com>
2022-05-16 17:05:38 -0700

    Monday Evening Changes

--- summary_calc.r
+++ summary_calc.r
@@ -1,7 +1,20 @@
 # This Doc for calculating summary info
 library(dplyr)
 
+# Making dfs and basic notes from park visits data
 park_visits <- read.csv("park_visits.csv")
-park_visits <- park_visits %>% mutate(visitors_millions = (10^logVisits)/1000000)
+park_visits <- park_visits %>% filter(ParkType == "National Park") %>% mutate(visitors_millions = (10^logVisits)/1000000)
 by_park_year <- park_visits %>% group_by(ParkName, Year) %>% summarise(total_vis = sum(visitors_millions))
 vis_by_region <- park_visits %>% group_by(Region) %>% summarise(total_vis = sum(visitors_millions))
+vis_by_state <- park_visits %>% group_by(State) %>% summarise(total_vis = sum(visitors_millions))
+
+most_vis_state <- (filter(vis_by_state, total_vis == max(vis_by_state$total_vis)))$State
+least_vis_state <- (filter(vis_by_state, total_vis == min(vis_by_state$total_vis)))$State
+
+most_vis_region <- (filter(vis_by_region, total_vis == max(vis_by_region$total_vis)))$Region
+least_vis_region <- (filter(vis_by_region, total_vis == min(vis_by_region$total_vis)))$Region
+
+most_vis_park <- filter(by_park_year, total_vis == max(by_park_year$total_vis)) %>% select(ParkName, Year)
+
+# Making dfs with parks data
+parks <- read.csv("parks.csv")
