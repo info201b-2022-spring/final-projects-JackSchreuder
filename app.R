4cee2315348ee196f4fa6559414adb89649d6f00
JackSchreuder <jackschreuder@icloud.com>
2022-05-30 10:35:55 -0700

    chart3 added (rough draft)

--- app.R
+++ app.R
@@ -2,8 +2,10 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 source("summary_calc.r")
 
+filtered_df <- vis_region_year
 
 ui <- fluidPage(
   titlePanel("Assesment of National Parks' Environmental Impact"),
@@ -18,7 +20,16 @@ ui <- fluidPage(
                        tableOutput("ch1_info")
                        ),
               tabPanel("Exploratory Chart 2"),
-              tabPanel("Exploratory Chart 3"),
+              tabPanel(title = "Exploratory Chart 3",
+                       h2("Explore regional visitation trends over time"),
+                       sidebarPanel(
+                         checkboxGroupInput(inputId = "regions",
+                                       label = h5("Choose which regions to include:"),
+                                       choices = vis_by_region$Region)
+                       ),
+                       mainPanel(
+                         plotOutput("exch3")
+                       )),
               tabPanel("Summary and Conclusion"))
 )
 
@@ -29,6 +40,11 @@ server <- function(input, output) {
   })
   output$ch1_info <- renderTable({
     nearPoints(vis_to_acre, input$ch1_click, xvar = "Acres", yvar = "total_vis")
+  })
+  output$exch3 <- renderPlot({
+    filtered_df <- vis_region_year %>% filter(Region %in% input$regions)
+    ggplot(filtered_df, aes(x=Year, y=total_vis, color=Region))+
+      geom_line()
   })
 }
 
