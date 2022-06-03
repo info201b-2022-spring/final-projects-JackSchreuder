a05e1df89028fc0f4a46572ac498b2e6bcf44008
JackSchreuder <jackschreuder@icloud.com>
2022-06-03 10:08:08 -0700

    included chart two and wrote conclusion

--- app.r
+++ app.r
@@ -3,7 +3,9 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(tidyverse)
 source("summary_calc.r")
+source("chart_2.r")
 
 filtered_df <- vis_region_year
 
@@ -30,7 +32,24 @@ ui <- fluidPage(
                        h6("Size of each point corresponds to the visitor / acres ratio"),
                        plotlyOutput("exch1"),
                        ),
-              tabPanel("Exploratory Chart 2"),
+              tabPanel("Exploratory Chart 2",
+                       sidebarLayout(
+                         sidebarPanel(
+                           h5("Controls"),
+                           selectInput(
+                             inputId = "value",
+                             label = "View state population or national park visitors",
+                             choices = list("Total NP visitors in 2016" = "total_visitor_2016", 
+                                            "State population in 2019" = "Population..2019."),
+                             multiple = FALSE,
+                             width = '50%',
+                           )
+                         ),
+                         mainPanel(
+                           plotlyOutput("chart2"),
+                           textOutput("chart2_text")
+                         )
+                       )),
               tabPanel(title = "Exploratory Chart 3",
                        h2("Explore regional visitation trends over time"),
                        sidebarPanel(
@@ -41,7 +60,31 @@ ui <- fluidPage(
                        mainPanel(
                          plotOutput("exch3")
                        )),
-              tabPanel("Summary and Conclusion"))
+              tabPanel(title = "Summary and Conclusion",
+                       h2("Conclusion and Analysis"),
+                       h4("What we learned from chart 1:"),
+                       p("The first thing we should note about chart 1 is that there is a ton of diversity in all
+                         the national parks in the USA. There are some that are really big that nobody goes to, there
+                         are some small that no one goes to, and there are just the opposite. In this project, we argued that
+                         parks with a low visitor to acreage ratio were the best in conserving land. On this chart, those are
+                         the points that are very small. Further, the points on the far right side of the graph best accomplish
+                         this, since they have the largest area, and thus conserve the largest amount of land."),
+                       h4("What we learned from chart 2:"),
+                       p("Things we learned..."),
+                       h4("What we learned from chart 3:"),
+                       p("Chart 3 is important because it shows that national park visitation is trending upward, which means
+                         more people are getting to see the beautiful parts of the country. This is generally a good thing for
+                         the NPS, since more people = more revenue. In terms of land conservation, this may not be a great thing.
+                         Especially given the large gap between the Intermountain and Southeast regions and the rest of the
+                         country. This means that the parks in those places are experiencing high volume visitors, and their land
+                         is likely being eroded quicker than in the Alaska region. Therefore, it can be argued that the regions
+                         with the flatest lines (ie the least change in visitation) are the most effective NPS regions."),
+                       h4("Overall:"),
+                       p("The National Parks Service is a very well run organization that achieves a great mix of making empty
+                         land available for the general public to enjoy and keeping large swaths of land free of development.
+                         In the end, we conclude that the NPS has achieved both the land conservation goal it publicized
+                         , but also the more subtle goal of making sure people got more in touch with their primal nature.")
+                       ))
 )
 
 server <- function(input, output) {
@@ -53,6 +96,57 @@ server <- function(input, output) {
         marker = list(color='green', size = ~vis_acre_ratio, sizeref = 4, sizemode = 'area'),
         showlegend = F
       )
+  })
+  
+  output$chart2 <- renderPlotly({
+    parks <- read.csv("parks.csv")
+    us_states_territories <- read.csv("us-states-territories.csv")
+    park_visits <- read.csv("park_visits.csv")
+    
+    us_states_territories <- us_states_territories %>% 
+      filter(Type == "State") %>% 
+      select(Abbreviation, Population..2019.)
+    
+    colnames(
+      us_states_territories)[
+        which(names(us_states_territories) == "Abbreviation")] <- "state"
+    
+    us_states_territories$state <- str_sub(us_states_territories$state, end=2)
+    us_states_territories$Population..2019. <- 
+      str_replace_all(us_states_territories$Population..2019., ',', '')
+    
+    total_visitor_state <- park_visits %>%
+      filter(Year == "2014", ParkType == "National Park") %>% 
+      group_by(State) %>%
+      summarise(total_visitor_2016 = sum(logVisits, na.rm = TRUE))
+    
+    
+    colnames(total_visitor_state)[1] <- "state"
+    
+    chart_2_df <- merge(
+      x=total_visitor_state,
+      y=us_states_territories,
+      by="state"
+    )
+    
+    if (input$value == "total_visitor_2016") {
+      chart_2_df <- chart_2_df %>% 
+        mutate(values = total_visitor_2016)
+      scale_name = "Visitors (persons)"
+    } else {
+      chart_2_df <- chart_2_df %>% 
+        mutate(values = as.numeric(Population..2019.))
+      scale_name = "Population (persons)"
+    }
+    
+    chart_2 <- plot_usmap(data = chart_2_df, regions = "states") +
+      scale_fill_continuous(
+        low = "light yellow", high = "dark red",
+        name = scale_name,
+        label = scales::comma) +
+      labs(title = "U.S. States") +
+      theme(legend.position = "right")
+    ggplotly(chart_2)
   })
 
   output$exch3 <- renderPlot({
